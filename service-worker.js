c50e1e9b39ff42c1c402dccdcd8e12e005f3a9bf
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2024-10-13 10:47:55 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,10 @@
-// Import Firebase scripts
-importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging.js');
-
-// Inicialize o Firebase no Service Worker
-const firebaseConfig = {
-  apiKey: "AIzaSyB4XSOOTIxbJvIpfd96MsyJZDW2aNi_uPc",
-  authDomain: "loto-hack.firebaseapp.com",
-  projectId: "loto-hack",
-  storageBucket: "loto-hack.appspot.com",
-  messagingSenderId: "138353732568",
-  appId: "1:138353732568:web:71f27a582f25cd544aa0ad",
-  measurementId: "G-P53RKCKPQ5"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// Obtém uma instância do Firebase Messaging para lidar com mensagens em background
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Recebida uma mensagem em background:', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icons/logo.png'
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-// PWA: Cache para o PWA
 const CACHE_NAME = 'loto-hack-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
     '/styles.css',
     '/script.js',
+    '/firebase-messaging.js',
     '/manifest.json',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
@@ -43,46 +13,51 @@ const urlsToCache = [
     '/icons/jogar.png',
     '/icons/palpite.png',
     '/icons/LotoHack.png',
+    '/icons/favicon.png',
+    '/icons/favicon.ico',
+    '/icons/favicon.svg',
+    // Adicione aqui outros recursos que você deseja cachear
 ];
 
 // Instala o Service Worker e adiciona os recursos ao cache
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aberto');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-// Intercepta as requisições e serve os recursos do cache
+// Intercepta as requisições e serve os recursos do cache quando disponíveis
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Retorna o recurso do cache, se encontrado
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Retorna o recurso do cache se encontrado
+                if (response) {
+                    return response;
+                }
+                // Caso contrário, busca na rede
+                return fetch(event.request);
+            })
+    );
 });
 
 // Atualiza o Service Worker e remove caches antigos
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            console.log('Deletando cache antigo:', cacheName);
-            return caches.delete(cacheName);
-          }
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
